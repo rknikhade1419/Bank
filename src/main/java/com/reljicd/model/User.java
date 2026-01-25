9f026d5bf719d2e86192858a256556a630d67d31
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-01-25 09:47:27 +0530

    Update User.java

--- src/main/java/com/reljicd/model/User.java
+++ src/main/java/com/reljicd/model/User.java
@@ -12,7 +12,70 @@ import java.util.Collection;
 @Table(name = "user")
 public class User {
 
+    @Idpackage com.reljicd.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    @Email(message = "*Please provide a valid Email")
+    @NotEmpty(message = "*Please provide an email")
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @Length(min = 5, message = "*Your password must have at least 5 characters")
+    @NotEmpty(message = "*Please provide your password")
+    @JsonIgnore
+    private String password;
+
+    @Column(name = "username", nullable = false, unique = true)
+    @Length(min = 5, message = "*Your username must have at least 5 characters")
+    @NotEmpty(message = "*Please provide your username")
+    private String username;
+
+    @Column(name = "name")
+    @NotEmpty(message = "*Please provide your name")
+    private String name;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "*Please provide your last name")
+    private String lastName;
+
+    @Column(name = "active", nullable = false)
+    private int active;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    // NEW: Link to Bank Accounts
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Collection<Account> accounts;
+
+    // ... Keep existing Getters/Setters ...
+
+    public Collection<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Collection<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private Long id;
