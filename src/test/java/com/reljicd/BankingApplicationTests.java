2747fc56b23cc8bcf216e310632ad84caf6b9ee6
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-01-25 09:02:48 +0530

    Update BankingApplicationTests.java

--- src/test/java/com/reljicd/BankingApplicationTests.java
+++ src/test/java/com/reljicd/BankingApplicationTests.java
@@ -7,10 +7,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShoppingCartApplicationTests {
+public class BankingApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        // This test will fail if any of your "Bank" configurations 
+        // (like the Database or Security) are broken.
+    }
 
 }
