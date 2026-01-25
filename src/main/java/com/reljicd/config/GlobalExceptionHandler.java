9579174923aaad4207427a84ff625eae52fc375d
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-01-25 09:23:25 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/reljicd/config/GlobalExceptionHandler.java
+++ src/main/java/com/reljicd/config/GlobalExceptionHandler.java
@@ -10,24 +10,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Global exception handler
- *
- * @author Dusan
+ * Global exception handler for the Banking System
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView exception(final Throwable throwable, final Model model) {
-        logger.error("Exception during execution of SpringSecurity application", throwable);
+        // Log the full error to the console/files for the developer to see
+        logger.error("Banking System Error: ", throwable);
 
         ModelAndView modelAndView = new ModelAndView("/error");
-        String errorMessage = (throwable != null ? throwable.toString() : "Unknown error");
-        modelAndView.addObject("errorMessage", errorMessage);
+        
+        // UPDATED: In a bank app, we want to be careful not to show internal code errors
+        // to the customer. We show a polite message instead.
+        String friendlyMessage = "We are currently experiencing technical difficulties. " +
+                                 "Rest assured, your funds are safe. Please try again later.";
+        
+        modelAndView.addObject("errorMessage", friendlyMessage);
         return modelAndView;
     }
-
 }
