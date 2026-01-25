7594139744b7dab0f215bf1cf7b019437cf48be7
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-01-25 09:40:05 +0530

    Update User.java

--- src/main/java/com/reljicd/model/User.java
+++ src/main/java/com/reljicd/model/User.java
@@ -30,7 +30,7 @@ public class User {
 
     @Column(name = "username", nullable = false, unique = true)
     @Length(min = 5, message = "*Your username must have at least 5 characters")
-    @NotEmpty(message = "*Please provide your name")
+    @NotEmpty(message = "*Please provide your username")
     private String username;
 
     @Column(name = "name")
@@ -48,67 +48,17 @@ public class User {
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Collection<Role> roles;
 
-    public Long getId() {
-        return id;
+    // NEW: Link to Bank Accounts
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Collection<Account> accounts;
+
+    // ... Keep existing Getters/Setters ...
+
+    public Collection<Account> getAccounts() {
+        return accounts;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getActive() {
-        return active;
-    }
-
-    public void setActive(int active) {
-        this.active = active;
-    }
-
-    public Collection<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Collection<Role> roles) {
-        this.roles = roles;
+    public void setAccounts(Collection<Account> accounts) {
+        this.accounts = accounts;
     }
 }
