a336777281d6d575269628842ef0db7cbd04772a
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-02-07 13:51:30 +0530

    Add pageable and findById methods to AccountService

--- src/main/java/com/reljicd/service/AccountService.java
+++ src/main/java/com/reljicd/service/AccountService.java
@@ -1,25 +1,6 @@
 package com.reljicd.service;
 
 import com.reljicd.model.Account;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface AccountService {
-    
-    List<Account> findAllAccounts();
-    
-    Optional<Account> findAccountById(Long id);
-    
-    Optional<Account> findAccountByAccountNumber(String accountNumber);
-    
-    Account saveAccount(Account account);
-    
-    void deleteAccount(Long id);
-}
-package com.reljicd.service;
-
-import com.reljicd.model.Account;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -38,8 +19,7 @@ public interface AccountService {
     
     void deleteAccount(Long id);
     
-    // ADD THESE TWO METHODS:
-    Account findById(Long id);  // For TransactionController
+    Account findById(Long id);
     
-    Page<Account> findAllAccountsPageable(Pageable pageable);  // For HomeController
+    Page<Account> findAllAccountsPageable(Pageable pageable);
 }
