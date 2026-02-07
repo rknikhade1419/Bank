122d6fef99ab59de83b8c992b472a138c8538368
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-02-07 13:58:53 +0530

    Reorder methods in TransactionService interface

--- src/main/java/com/reljicd/service/TransactionService.java
+++ src/main/java/com/reljicd/service/TransactionService.java
@@ -6,23 +6,13 @@ import com.reljicd.model.Account;
 import java.math.BigDecimal;
 import java.util.Map;
 
-/**
- * Service to handle the logic of moving funds between accounts.
- */
 public interface TransactionService {
-
-    // Stages an account for a transfer (similar to adding to cart)
-    void selectAccountForTransfer(Account account);
-
-    // Removes an account from the current transaction staging
-    void deselectAccount(Account account);
-
-    // Returns the accounts involved in the current pending transaction
+    
     Map<Account, Integer> getAccountsInSummary();
-
-    // The banking equivalent of "checkout"
+    
+    void selectAccountForTransfer(Account account);
+    
     void executeTransfer() throws InsufficientFundsException;
-
-    // Calculates the total value of the pending transfer or net worth
+    
     BigDecimal getTotalNetWorth();
 }
